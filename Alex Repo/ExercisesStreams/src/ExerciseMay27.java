ce90caa8dd79eb63fa9ec4c1b02d55e0e48a70de
Alex <oleksandr.ovcharenko@dci-student.org>
2024-05-28 09:26:40 +0200

    May 27 Exercises

--- Alex Repo/ExercisesStreams/src/ExerciseMay27.java
+++ Alex Repo/ExercisesStreams/src/ExerciseMay27.java
@@ -9,27 +9,26 @@ import java.util.stream.Collectors;
 public class ExerciseMay27 {
     public static void main(String[] args) throws URISyntaxException, FileNotFoundException {
         URL sampleUrl = ExerciseMay27.class.getResource("sample.txt");
+        assert sampleUrl != null;
         URI sampleUri = sampleUrl.toURI();
         File sampleFile = new File(sampleUri);
         Scanner input = new Scanner(sampleFile);
+
         List<String> paragraphs = new ArrayList<>();
 
         while (input.hasNext()) {
             String line = input.nextLine().trim();
-            paragraphs.add(line);
-        }
-
-        for (int i = paragraphs.size() - 1; i >= 0; i--) {
-            if (paragraphs.get(i).isEmpty()) {
-                paragraphs.remove(i);
+            if (!line.isEmpty()) {
+                paragraphs.add(line);
             }
         }
+        input.close();
 
         List<List<String>> fileContent = new ArrayList<>();
         fileContent.add(paragraphs);
 
-        System.out.println("There is " + fileContent.getFirst().size() + " paragraph" + (fileContent.getFirst().size() == 1 ? "" : "s") +
-                " in the file.");
+        int counter = fileContent.getFirst().size();
+        System.out.println("There is " + counter + " paragraph" + (counter == 1 ? "" : "s") + " in the file.");
 
         List<String> sentences = new ArrayList<>();
 
@@ -37,16 +36,10 @@ public class ExerciseMay27 {
             sentences.addAll(Arrays.asList(paragraph.split("\\.")));
         }
 
-        for (int i = sentences.size() - 1; i >= 0; i--) {
-            if (sentences.get(i).isEmpty()) {
-                sentences.remove(i);
-            }
-        }
-
         fileContent.add(sentences);
 
-        System.out.println("There is " + fileContent.get(1).size() + " sentence" + (fileContent.get(1).size()  == 1 ? "" : "s") +
-                " in the file.");
+        counter = fileContent.get(1).size();
+        System.out.println("There is " + counter + " sentence" + (counter == 1 ? "" : "s") + " in the file.");
 
         List<String> words = new ArrayList<>();
 
@@ -61,8 +54,8 @@ public class ExerciseMay27 {
 
         fileContent.add(words);
 
-        System.out.println("There is " + fileContent.get(2).size() + " word" + (fileContent.get(2).size() == 1 ? "" : "s") +
-                " in the file.");
+        counter = fileContent.get(2).size();
+        System.out.println("There is " + counter + " word" + (counter == 1 ? "" : "s") + " in the file.");
 
         // Bonus
 
